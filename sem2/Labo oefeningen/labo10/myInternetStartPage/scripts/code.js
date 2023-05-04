b92e8ebb8d7eaaf113152aae335bfe2ec2c80475
LukaDevlieger <Luka.Devlieger@student.vives.be>
2023-05-04 19:10:41 +0200

    bug fixes

--- sem2/Labo oefeningen/labo10/myInternetStartPage/scripts/code.js
+++ sem2/Labo oefeningen/labo10/myInternetStartPage/scripts/code.js
@@ -11,8 +11,7 @@ const setup = () => {
 const checkCommand = () => {
     let input = document.getElementById("commandoInput").value;
     input = input.toLowerCase().trim();
-
-    let command = input.slice(0,2);
+    console.log("here")
 
     let search = input.substring(3);
 
@@ -21,7 +20,13 @@ const checkCommand = () => {
     let h = {};
 
     let arrayget = localStorage.getItem("MyInternetStartPage");
-    let array = JSON.parse(arrayget);
+    array = JSON.parse(arrayget);
+
+    if (array == null){
+        array = [];
+    }
+
+    console.log(array);
 
     if (input.startsWith("/g")){
         url = "https://www.google.com/search?q=";
@@ -64,13 +69,16 @@ const checkCommand = () => {
 
 
 
-    if (url !== ""){
-        window.open(url+search);
+    if (h !== {}){
+        window.open(url + search);
         array.push(h);
+        console.log(h);
     }
 
     arrayJSON = JSON.stringify(array);
     localStorage.setItem("MyInternetStartPage",arrayJSON);
+
+    buildHistory();
 
 }
 
@@ -78,66 +86,75 @@ const buildHistory = () =>{
     let arrayJSON = localStorage.getItem("MyInternetStartPage");
     let arrayget = JSON.parse(arrayJSON);
 
-    for ( let i = 0; i < arrayget.length ; i++){
-        let items = arrayget[i];
-
-        let col = createElement("div","col-4");
-
-
-        let e = null;
-        if (items.title == "youtube"){
-            //de juiste classes geven aan de elementen
-            // ook van de andere site dingen dus
-            e = createElement("div","card youtube-card");
-        } else if(items.title == "instagram"){
-            e = createElement("div","card instagram-card");
-        } else if(items.title == "google") {
-            e = createElement("div", "card google-card");
-        } else if(items.title == "twitter"){
-            e = createElement("div","card twitter-card");
-        }
-
-
-        col.appendChild(e);
-
-        let divbody = createElement("div","card-body");
-         e.appendChild(divbody);
-
-         let h5 = createElementWithText("h5","card-title",items.title);
-         divbody.appendChild(h5);
-
-         let p = createElementWithText("p","card-text",items.text);
-         divbody.appendChild(p);
-
-
-         let a = null;
-
-        if (items.title == "youtube"){
-            a = createElementWithText("a","btn btn-primary youtube-button","GO!");
-            a.setAttribute("href",items.url);
-        } else if(items.title == "instagram"){
-            a = createElementWithText("a","btn btn-primary instagram-button","GO!");
-            a.setAttribute("href",items.url);
-        } else if(items.title == "google") {
-            a = createElementWithText("a","btn btn-primary google-button","GO!");
-            a.setAttribute("href",items.url);
-        } else if(items.title == "twitter"){
-            a = createElementWithText("a","btn btn-primary twitter-button","GO!");
-            a.setAttribute("href",items.url);
-        }
-
-
-
-         divbody.appendChild(a);
-
-
-
-         kaarten = document.getElementsByClassName("row");
-         kaarten[0].appendChild(col);
-
-
-
+    kaarten = document.getElementsByClassName("row");
+    while (kaarten[0].firstChild) {
+        kaarten[0].removeChild(kaarten[0].firstChild);
     }
+
+    if (arrayget != null){
+        console.log(arrayget.length);
+        for ( let i = arrayget.length -1; i >= 0 ; i--){
+            let items = arrayget[i];
+
+            let col = createElement("div","col-4");
+
+            let e = null;
+            if (items.title == "youtube"){
+                //de juiste classes geven aan de elementen
+                // ook van de andere site dingen dus
+                e = createElement("div","card youtube-card");
+            } else if(items.title == "instagram"){
+                e = createElement("div","card instagram-card");
+            } else if(items.title == "google") {
+                e = createElement("div", "card google-card");
+            } else if(items.title == "twitter"){
+                e = createElement("div","card twitter-card");
+            }
+
+
+            col.appendChild(e);
+
+            let divbody = createElement("div","card-body");
+            e.appendChild(divbody);
+
+            let h5 = createElementWithText("h5","card-title",items.title);
+            divbody.appendChild(h5);
+
+            let p = createElementWithText("p","card-text",items.text);
+            divbody.appendChild(p);
+
+
+            let a = null;
+
+            if (items.title == "youtube"){
+                a = createElementWithText("a","btn btn-primary youtube-button","GO!");
+                a.setAttribute("href",items.url);
+            } else if(items.title == "instagram"){
+                a = createElementWithText("a","btn btn-primary instagram-button","GO!");
+                a.setAttribute("href",items.url);
+            } else if(items.title == "google") {
+                a = createElementWithText("a","btn btn-primary google-button","GO!");
+                a.setAttribute("href",items.url);
+            } else if(items.title == "twitter"){
+                a = createElementWithText("a","btn btn-primary twitter-button","GO!");
+                a.setAttribute("href",items.url);
+            }
+
+
+
+            divbody.appendChild(a);
+
+
+
+
+            kaarten[0].appendChild(col);
+
+
+
+        }
+    }
+
+
 }
 
 const createElement =(element,classname) =>{
@@ -151,4 +168,5 @@ const createElementWithText = (element,classname,text) =>{
     e.appendChild(document.createTextNode(text));
     return e;
 }
+
 window.addEventListener("load", setup);
